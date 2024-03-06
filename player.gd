8e7df7e58cb64c9b7154bab474eaf33da3c6074b
Colin-Paslay <148103002+Colin-Paslay@users.noreply.github.com>
2024-03-06 11:14:27 -0600

    Fonts Suck
    agggggggggggggg

--- player.gd
+++ player.gd
@@ -2,18 +2,15 @@ extends CharacterBody2D
 
 const SPEED = 64.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var directionx = Input.get_axis("ui_left", "ui_right")
 	var directiony = Input.get_axis("ui_up", "ui_down")
-	if directionx == -1: 
+	if directionx < 0: 
 		$AnimatedSprite2D.play("WalkingLeft")
-	elif directionx == 1: $AnimatedSprite2D.play("WalkingRight")
+	elif directionx > 0: $AnimatedSprite2D.play("WalkingRight")
 	else: pass
-	if directiony == 1: $AnimatedSprite2D.play("WalkingForward")
-	elif directiony == -1: $AnimatedSprite2D.play("WalkingBackward")
+	if directiony > 0: $AnimatedSprite2D.play("WalkingForward")
+	elif directiony < 0: $AnimatedSprite2D.play("WalkingBackward")
 	else: pass
 	if directionx or directiony:
 		velocity.x = directionx * SPEED
