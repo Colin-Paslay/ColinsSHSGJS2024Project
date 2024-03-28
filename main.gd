8acc43b5c1192326f4676d2aab41ca71542093b5
Colin-Paslay <148103002+Colin-Paslay@users.noreply.github.com>
2024-03-28 10:43:06 -0500

    Fixed Area 2d
    I thought the general "area entered" would work, but you need to use "body entered"

--- main.gd
+++ main.gd
@@ -1,10 +1,12 @@
 extends Node2D
+@onready var interior = $Interior
+@onready var exterior = $Exterior
 
 func _process(delta):
 	pass
-func _on_area_2d_area_entered(area):
-	$Interior.show()
-	$Exterior.hide()
-func _on_area_2d_area_exited(area):
-	$Interior.hide()
-	$Exterior.show()
+func _on_area_2d_body_entered(body):
+	interior.show()
+	exterior.hide()
+func _on_area_2d_body_exited(body):
+	interior.hide()
+	exterior.show()
